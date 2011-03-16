84d67df408904b90979db4d56520f830882dbc09
Clint Ecker <me@clintecker.com>
2011-03-16 18:41:28 -0500

    Adding shorturl to this

---
+++ shorturl/worker-development.rb
@@ -0,0 +1,13 @@
+require 'worker'
+require 'exceptional'
+
+Exceptional::Config.load("config/exceptional.yml")
+
+Exceptional.rescue_and_reraise do
+  Worker.mongo = Mongo::Connection.multi([['10.10.10.2', 27017], ['10.10.10.10', 27017], ['10.10.10.9', 27017]]).db('ars')
+  Worker.beanstalk = Beanstalk::Pool.new(['10.10.10.11:11300'], 'shorturl')
+  Worker.debug = true
+  Worker.restart_file_path = "./tmp/restart.txt"
+  Worker.worker_log = "./tmp/worker.log"
+  Worker.run
+end
