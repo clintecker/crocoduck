989f1782fd4342830c71bf029f086eaa2033656c
Clint Ecker <me@clintecker.com>
2011-04-17 18:18:40 -0500

    Lazy load stuff and cache.  Also make id_field be a class var and also implement an update function to mutate docs

--- lib/crocoduck/store.rb
+++ lib/crocoduck/store.rb
@@ -4,25 +4,35 @@ require 'crocoduck/logging'
 module Crocoduck
   class Store
     include Logging
+    @id_field = '_id'
     
-    attr_accessor :store, :entries
-    def initialize(mongo_cluster, db_name)
-      @mongo = Mongo::Connection.multi(mongo_cluster).db(db_name)
+    attr_accessor :store, :entries, :collection, :db_name, :mongo_cluster
+    def initialize(mongo_cluster, db_name1, collection)
+      @collection = collection
+      @db_name = db_name
+      @mongo_cluster = mongo_cluster
     end
     
     def entries
-      @entries ||= store.collection('entries')
+      @entries ||= store.collection collection
     end
     
-    def put(entry_id, data)
+    def update(entry, field, value)
+      entries.update({id_field => entry[id_field]}, {'$set' => { field => value}}, :safe => true)
     end
-    
+        
     def get(entry_id)
-      entries.find_one({'_id' => entry_id.to_i})
+      entries.find_one({id_field => entry_id.to_i})
     end
     
     def close
       store.close
+    end
+    
+    private
+    
+    def store
+      @store ||= Mongo::Connection.multi(mongo_cluster).db(db_name)
     end
   end
 end
