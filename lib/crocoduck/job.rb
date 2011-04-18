f9b245bf9b85155a8e9091af93e8e499c57453f9
Clint Ecker <me@clintecker.com>
2011-04-18 10:17:57 -0500

    Make job consistent with changes to the Entry and Store classes.

--- lib/crocoduck/job.rb
+++ lib/crocoduck/job.rb
@@ -1,4 +1,5 @@
 require 'crocoduck/logging'
+require 'crocoduck/entry'
 
 module Crocoduck
   class Job
@@ -7,15 +8,13 @@ module Crocoduck
     # Interface with Resque, finds the
     #  right entry object, instantiates
     #  a new job and calls `run` on it.
-    def perform(entry_id)
-      if entry = Entry.find(entry_id)
-        new(entry).run
-      end
+    def self.perform(entry_id)
+      new(Entry.new entry_id).run
     end
     
     include Logging
     
-    attr_accessor :entry
+    attr_accessor :entry, :store
   
     def initialize(entry)
       @entry = entry
@@ -23,22 +22,25 @@ module Crocoduck
     
     def do_work
       # Do Something with entry
+      entry.update "derp", "herp"
       # shorturl = shorturl.generate @entry.url
       # store.update entry_id, 'shorturl', shorturl
       # store.update entry_id, 'shorturl_status, job_status
-      
     end
     
     # A thin wrapper around the real workhorse (do_work)
     def run
-      return unless entry.setup?
-      self.store = entry.store
-      benchmark :info, "Running job" { do_work }
+      store = entry.store
+      return unless store.setup?
+      return unless entry.entry
+      benchmark :info, "Running job" do
+        do_work
+      end
     rescue Exception =>e
-      entry.save
+      # Clean up entry (not really needed in our case)
       raise e
     else
-      entry.save
+      # Do anything you'd need to do once the processing was finished
     ensure
       # Clean up mongo and anything else
       store.close
