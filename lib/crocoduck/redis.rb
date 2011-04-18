03af678e50ac2b921059aebaab94b3c018713b70
Clint Ecker <me@clintecker.com>
2011-04-18 10:16:41 -0500

    This is hardcoded, but will probably go away.  We don't need to talk to redis directly anymore (but the resque module it using this for configuration so we might just move this configuration into the config environment files).

--- lib/crocoduck/redis.rb
+++ lib/crocoduck/redis.rb
@@ -2,6 +2,6 @@ require 'redis'
 require 'uri'
 
 module Crocoduck
-  uri = URI.parse ENV['REDIS_URL']
-  Redis = ::Redis.new host: uri.host, port: uri.port, password: uri.password
+  uri = URI.parse "http://localhost:6379"
+  Redis = ::Redis.new({ :host => uri.host, :port => uri.port, :password => uri.password })
 end
