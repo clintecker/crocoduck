6bccf4170c1a13cb2047c2e8669b3d8d5ad5dd48
Clint Ecker <me@clintecker.com>
2011-04-24 14:47:21 -0500

    Cleanup environments

--- config/production.rb
+++ config/production.rb
@@ -1,3 +1,5 @@
+require 'crocoduck/job'
+
 Crocoduck::Store.server_cluster = [['10.10.10.2', 27017], ['10.10.10.10', 27017], ['10.10.10.9', 27017]]
 Crocoduck::Store.server_db = 'ars'
 Crocoduck::Store.server_collection = 'entries'
